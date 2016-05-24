53d186b296f95a9d72ea7e62f7d12afd722a223f
nathanlyons <nathan.j.lyons@gmail.com>
2016-05-24 08:51:36 -0400

    Delete parameters.py

--- streambed/stream/parameters.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-class Parameters(object):
-    
-    self.waterDensity = 1000
-    self.gravitationalAcceleration = 9.81    
-    
-    def __init__(self, sedimentDensity=2650):
-        self.sedimentDensity = sedimentDensity
